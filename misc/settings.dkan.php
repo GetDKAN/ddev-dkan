a9feb624818953ec331eae08e9b479af5e579f84
Paul Mitchum <paul@mile23.com>
2022-08-01 17:01:00 -0700

    host command dkan-init

---
+++ misc/settings.dkan.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * #ddev-generated: Automatically generated Drupal settings file.
+ * DKAN-DDev-addon manages this file and may delete or overwrite the file unless this
+ * comment is removed.  It is recommended that you leave this file alone.
+ */
+
+if ($primary_url = getenv('DDEV_PRIMARY_URL')) {
+  $settings['file_public_base_url'] = $primary_url;
+}
